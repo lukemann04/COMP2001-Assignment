f8ac50daf4f0239ab578650b4cebb01158dc3a87
lukemann04 <34482771+lukemann04@users.noreply.github.com>
2021-01-16 14:45:46 +0000

    Set up API
    Set up the API application and created a Users table controller

---
+++ COMP2001-Coursework-API/COMP2001-Coursework-API/Models/CourseworkSession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001_Coursework_API.Models
+{
+    public partial class CourseworkSession
+    {
+        public int SessionId { get; set; }
+        public int? UserId { get; set; }
+        public DateTime SessionTime { get; set; }
+
+        public virtual CourseworkUser User { get; set; }
+    }
+}
---
+++ COMP2001-Coursework-API/COMP2001-Coursework-API/Models/LogInCount.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001_Coursework_API.Models
+{
+    public partial class LogInCount
+    {
+        public int UserId { get; set; }
+        public int? VisitCount { get; set; }
+    }
+}
