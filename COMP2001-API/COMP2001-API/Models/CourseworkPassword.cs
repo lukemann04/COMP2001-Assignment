1ab6d69bc21dd3bb4891623afb7cd51611fa867d
lukemann04 <34482771+lukemann04@users.noreply.github.com>
2021-01-18 15:19:24 +0000

    Fixed 404 error
    Rebuilt api to correctly display when run.

---
+++ COMP2001-API/COMP2001-API/Models/CourseworkPassword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001_API.Models
+{
+    public partial class CourseworkPassword
+    {
+        public int PasswordId { get; set; }
+        public int UserId { get; set; }
+        public string OldPassword { get; set; }
+        public DateTime ChangeTime { get; set; }
+
+        public virtual CourseworkUser User { get; set; }
+    }
+}
---
+++ COMP2001-API/COMP2001-API/Models/CourseworkUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace COMP2001_API.Models
+{
+    public partial class CourseworkUser
+    {
+        public CourseworkUser()
+        {
+            CourseworkSessions = new HashSet<CourseworkSession>();
+        }
+
+        public int UserId { get; set; }
+        public string UserFirstName { get; set; }
+        public string UserLastName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPassword { get; set; }
+
+        public virtual CourseworkPassword CourseworkPassword { get; set; }
+        public virtual ICollection<CourseworkSession> CourseworkSessions { get; set; }
+    }
+}
